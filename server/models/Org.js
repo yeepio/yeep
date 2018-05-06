16fcfbe2bb2a861bf52b50e3efb86fc00b6cba22
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-05-06 12:03:34 +0300

    Create Org model (WIP)

---
+++ server/models/Org.js
@@ -0,0 +1,43 @@
+import { Schema, model as createModel } from 'mongoose';
+
+const orgSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 100,
+      minlength: 2,
+    },
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 30,
+      minlength: 2,
+      lowercase: true,
+    },
+  },
+  {
+    collection: 'orgs',
+    autoIndex: false,
+    bufferCommands: false,
+    _id: true, // enable _id PK
+    id: true, // i.e. create `id` getter to retrieve _id in hex format
+    minimize: false, // allow empty object
+    strict: true, // reject values not specified in schema
+    validateBeforeSave: true,
+    versionKey: '_v',
+    timestamps: true,
+  }
+);
+
+orgSchema.index(
+  { slug: 1 },
+  {
+    unique: true,
+    name: 'org_slug_uidx',
+  }
+);
+
+export default createModel('Org', orgSchema);
