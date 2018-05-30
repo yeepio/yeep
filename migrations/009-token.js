baea5776d6198ebe6561af68a5c4ddd4c218fdda
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-05-30 09:44:10 +0300

    Add token migration to set expiresAt idx

---
+++ migrations/009-token.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+exports.up = async function(next) {
+  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connection.db
+    .collection('tokens')
+    .createIndex(
+      { expiresAt: 1 },
+      { name: 'expiresAt_idx', expireAfterSeconds: 0 },
+      next
+    );
+};
+
+exports.down = async function(next) {
+  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connection.db
+    .collection('tokens')
+    .dropIndex('expiresAt_idx', next);
+};
