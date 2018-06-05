0b6a8216c945d5c89539966be4e9a56c25b49c34
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-06-06 00:05:46 +0300

    Better session.create() test descriptions

--- server/api/session/create/controller.test.js
+++ server/api/session/create/controller.test.js
@@ -11,7 +11,7 @@ describe('api/v1/session.create', () => {
     await server.teardown();
   });
 
-  test('throws 400 "Bad Request" when username is invalid', async () => {
+  test('returns error when username is invalid', async () => {
     const res = await request(server)
       .post('/api/v1/session.create')
       .send({
@@ -30,7 +30,7 @@ describe('api/v1/session.create', () => {
     });
   });
 
-  test('throws "UserNotFoundError" error when user does not exist', async () => {
+  test('returns error when user does not exist', async () => {
     const res = await request(server)
       .post('/api/v1/session.create')
       .send({
@@ -48,7 +48,7 @@ describe('api/v1/session.create', () => {
     });
   });
 
-  test('throws "InvalidCredentialsError" error when password is invalid', async () => {
+  test('returns error when password is invalid', async () => {
     let res = await request(server)
       .post('/api/v1/user.create')
       .send({
@@ -88,33 +88,6 @@ describe('api/v1/session.create', () => {
     expect(res.status).toBe(204);
   });
 
-  // test('throws 422 "Bad Request" when multiple primary emails are specified', async () => {
-  //   const res = await request(server)
-  //     .post('/api/v1/session.create')
-  //     .send({
-  //       username: 'wile',
-  //       password: 'catch-the-b1rd$',
-  //       fullName: 'Wile E. Coyote',
-  //       emails: [
-  //         {
-  //           address: 'coyote@acme.com',
-  //           isVerified: false,
-  //           isPrimary: true,
-  //         },
-  //         {
-  //           address: 'wile@acme.com',
-  //           isVerified: false,
-  //           isPrimary: true,
-  //         },
-  //       ],
-  //     });
-
-  //   expect(res.status).toBe(422);
-  //   expect(res.body.message).toBe(
-  //     'User cannot have more than 1 primary emails'
-  //   );
-  // });
-
   test('creates new session', async () => {
     let res = await request(server)
       .post('/api/v1/user.create')
@@ -153,93 +126,4 @@ describe('api/v1/session.create', () => {
       .send({ id: userId });
     expect(res.status).toBe(204);
   });
-
-  // test('throws 409 "Conflict" on duplicate username', async () => {
-  //   let res = await request(server)
-  //     .post('/api/v1/session.create')
-  //     .send({
-  //       username: 'Wile',
-  //       password: 'catch-the-b1rd$',
-  //       fullName: 'Wile E. Coyote',
-  //       emails: [
-  //         {
-  //           address: 'coyote@acme.com',
-  //           isVerified: true,
-  //           isPrimary: true,
-  //         },
-  //       ],
-  //     });
-  //   expect(res.status).toBe(201);
-
-  //   const { id } = res.body.user;
-
-  //   res = await request(server)
-  //     .post('/api/v1/session.create')
-  //     .send({
-  //       username: 'wile',
-  //       password: 'play-wolf',
-  //       fullName: 'Road Runner',
-  //       emails: [
-  //         {
-  //           address: 'beep-beep@acme.com',
-  //           isVerified: true,
-  //           isPrimary: true,
-  //         },
-  //       ],
-  //     });
-  //   expect(res.status).toBe(409);
-  //   expect(res.body.message).toMatch(/username/i);
-
-  //   res = await request(server)
-  //     .post('/api/v1/session.delete')
-  //     .send({ id });
-  //   expect(res.status).toBe(204);
-  // });
-
-  // test('throws 409 "Conflict" on duplicate email address', async () => {
-  //   let res = await request(server)
-  //     .post('/api/v1/session.create')
-  //     .send({
-  //       username: 'Wile',
-  //       password: 'catch-the-b1rd$',
-  //       fullName: 'Wile E. Coyote',
-  //       emails: [
-  //         {
-  //           address: 'coyote@acme.com',
-  //           isVerified: true,
-  //           isPrimary: true,
-  //         },
-  //         {
-  //           address: 'carnivorous@acme.com',
-  //           isVerified: false,
-  //           isPrimary: false,
-  //         },
-  //       ],
-  //     });
-  //   expect(res.status).toBe(201);
-
-  //   const { id } = res.body.user;
-
-  //   res = await request(server)
-  //     .post('/api/v1/session.create')
-  //     .send({
-  //       username: 'roadrunner',
-  //       password: 'play-wolf',
-  //       fullName: 'Road Runner',
-  //       emails: [
-  //         {
-  //           address: 'Carnivorous@acme.com',
-  //           isVerified: true,
-  //           isPrimary: true,
-  //         },
-  //       ],
-  //     });
-  //   expect(res.status).toBe(409);
-  //   expect(res.body.message).toMatch(/email address/i);
-
-  //   res = await request(server)
-  //     .post('/api/v1/session.delete')
-  //     .send({ id });
-  //   expect(res.status).toBe(204);
-  // });
 });
