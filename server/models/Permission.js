9f3c178769ba3e0bf4d9aa1619955a8e6f740ef6
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-06-09 18:03:11 +0300

    Introduce permission model

---
+++ server/models/Permission.js
@@ -0,0 +1,33 @@
+import { Schema } from 'mongoose';
+
+const permissionSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 2,
+      maxlength: 64,
+    },
+    description: {
+      type: String,
+      required: false,
+      trim: true,
+      maxlength: 140,
+    },
+  },
+  {
+    collection: 'permissions',
+    autoIndex: false,
+    bufferCommands: false,
+    _id: true, // enable _id PK
+    id: false, // i.e. create `id` getter to retrieve _id in hex format
+    minimize: false, // allow empty object
+    strict: true, // reject values not specified in schema
+    validateBeforeSave: true,
+    versionKey: '_v',
+    timestamps: true,
+  }
+);
+
+export default permissionSchema;
