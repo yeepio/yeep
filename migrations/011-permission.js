2cac7697a4a5c71b4139785da9c414e3351939f0
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-06-09 18:41:04 +0300

    Add timestamps to migrations/011-permission.js

--- migrations/011-permission.js
+++ migrations/011-permission.js
@@ -2,15 +2,20 @@ const mongoose = require('mongoose');
 
 exports.up = async function(next) {
   await mongoose.connect(process.env.MONGODB_URI);
+  const now = new Date();
   await mongoose.connection.db.collection('permissions').insertMany(
     [
       {
         _id: 'yeep.user.write',
         description: 'Permission to write (i.e. create, update, delete) users',
+        createdAt: now,
+        updatedAt: now,
       },
       {
         _id: 'yeep.user.read',
         description: 'Permission to read users',
+        createdAt: now,
+        updatedAt: now,
       },
     ],
     next
