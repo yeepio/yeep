99b2906c4eede6905dcfa467b12cec4bb10d167b
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-08-17 09:10:14 +0300

    Update User model; make username optional

--- server/models/User.js
+++ server/models/User.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: false,
       trim: true,
       maxlength: 30,
       minlength: 2,
