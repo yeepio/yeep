d98d6cd6c243ea04c8035a7c339151b52fa63ada
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-08-23 09:37:21 +0300

    Add `Role` to models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,8 @@
 export { default as Org } from './Org';
 export { default as User } from './User';
+export { default as Credentials } from './Credentials';
 export { default as Token } from './Token';
 export { default as Permission } from './Permission';
 export { default as PermissionAssignment } from './PermissionAssignment';
+export { default as Role } from './Role';
 export { default as Settings } from './Settings';
-export { default as Credentials } from './Credentials';
