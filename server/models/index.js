0c1aee83ebcfe37b346a0b6b2f52c51effc7e191
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-08-25 11:29:20 +0300

    Add RoleAssignment to models index

--- server/models/index.js
+++ server/models/index.js
@@ -5,4 +5,5 @@ export { default as Token } from './Token';
 export { default as Permission } from './Permission';
 export { default as PermissionAssignment } from './PermissionAssignment';
 export { default as Role } from './Role';
+export { default as RoleAssignment } from './RoleAssignment';
 export { default as Settings } from './Settings';
