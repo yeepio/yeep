c763ff58a20cae59d9d8428bd5c1912e672c2644
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-10-08 09:31:31 +0300

    Introduce multipartForm middleware

---
+++ server/middleware/multipartForm.js
@@ -0,0 +1,36 @@
+import os from 'os';
+import formidable from 'formidable';
+
+formidable.IncomingForm.prototype.parseAsync = function(req) {
+  return new Promise((resolve, reject) => {
+    this.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({ fields, files });
+    });
+  });
+};
+
+export const parseMultipartForm = ({
+  encoding = 'utf-8',
+  uploadDir = os.tmpdir(),
+  keepExtensions = false,
+  maxFieldsSize = 20 * 1024 * 1024, // i.e. 20M
+  maxFields = 100,
+  hash = false,
+  multiples = false,
+} = {}) => async ({ req, request }, next) => {
+  const form = new formidable.IncomingForm();
+  form.encoding = encoding;
+  form.uploadDir = uploadDir;
+  form.keepExtensions = keepExtensions;
+  form.maxFieldsSize = maxFieldsSize;
+  form.maxFields = maxFields;
+  form.hash = hash;
+  form.multiples = multiples;
+  request.body = await form.parseAsync(req);
+  await next();
+};
