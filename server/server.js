5766abc19b3b5c259352dfc743c12c99a6e3aaca
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-10-22 23:14:44 +0300

    Disable mongoose debug mode in development

--- server/server.js
+++ server/server.js
@@ -41,11 +41,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(koaConnect(compression()));
 }
 
-// check if in development mode
-if (process.env.NODE_ENV === 'development') {
-  mongoose.set('debug', true);
-}
-
 // handle errors
 app.use(errorHandler);
 
