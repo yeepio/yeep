d280330c0030e9d2fcf721eb1ab24362df25fa70
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-10-28 08:01:56 +0200

    Update migration to set default `passwordResetEmailTemplate` prop

--- migrations/021-settings.js
+++ migrations/021-settings.js
@@ -1,11 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 const mongoose = require('mongoose');
 
+const readFileAsync = promisify(fs.readFile);
+
 exports.up = async function(next) {
+  const passwordResetEmailTemplate = await readFileAsync(
+    path.resolve(__dirname, '../server/views/passwordReset.html'),
+    {
+      encoding: 'utf8',
+    }
+  );
   await mongoose.connect(process.env.MONGODB_URI);
   await mongoose.connection.db.collection('settings').insertOne(
     {
       isUsernameEnabled: true,
       isOrgCreationOpen: true,
+      passwordResetEmailTemplate,
     },
     next
   );
