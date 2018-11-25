622fa9f3aa6be2d039767217ca13f8dfa3579e27
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-11-25 22:29:49 +0200

    Add build config for migrations

--- rollup.config.js
+++ rollup.config.js
@@ -5,56 +5,84 @@ import executable from 'rollup-plugin-executable';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import esModuleInterop from 'rollup-plugin-es-module-interop';
+import copy from 'rollup-plugin-copy';
+import glob from 'glob';
 
-const serverConfig = {
-  input: 'server/server.js',
-  output: [
-    {
-      file: 'dist/server.js',
-      format: 'cjs',
-    },
-  ],
-  plugins: [
-    resolve({
-      only: [/^\.{0,2}\//],
-    }),
-    commonjs(),
-    esModuleInterop(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    progress({
-      clearLine: false,
-    }),
-  ],
-};
-
-const cliConfig = {
-  input: 'cli/cli.js',
-  output: [
-    {
-      banner: '#!/usr/bin/env node',
-      file: 'dist/yeep.js',
-      format: 'cjs',
-    },
-  ],
-  plugins: [
-    replace({
-      '../server/server': './server',
-      delimiters: ['', ''],
-    }),
-    resolve({
-      only: [/^\.{0,2}\//],
-    }),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    executable(),
-    progress({
-      clearLine: false,
-    }),
-  ],
-};
-
-export default [serverConfig, cliConfig];
+export default [
+  /* server.js configuration */
+  {
+    input: 'server/server.js',
+    output: [
+      {
+        file: 'dist/server.js',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      resolve({
+        only: [/^\.{0,2}\//],
+      }),
+      commonjs(),
+      esModuleInterop(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      progress({
+        clearLine: false,
+      }),
+    ],
+  },
+  /* yeep CLI configuration */
+  {
+    input: 'cli/cli.js',
+    output: [
+      {
+        banner: '#!/usr/bin/env node',
+        file: 'dist/yeep.js',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      copy({
+        'server/views': 'dist/views',
+        verbose: true,
+      }),
+      replace({
+        '../server/server': './server',
+        '../migrations': './migrations',
+        delimiters: ['', ''],
+      }),
+      resolve({
+        only: [/^\.{0,2}\//],
+      }),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      executable(),
+      progress({
+        clearLine: false,
+      }),
+    ],
+  },
+  /* migrations build */
+  ...glob.sync('migrations/*.js').map((file) => ({
+    input: file,
+    output: [
+      {
+        file: file.replace('migrations', 'dist/migrations'),
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      replace({
+        '../server/views': '../views',
+        delimiters: ['', ''],
+      }),
+    ],
+  })),
+];
