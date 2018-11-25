d4e112b2345a5876266b72e808454c875ce2ee94
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-11-25 21:00:12 +0200

    Enable dynamic-import with babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -9,5 +9,8 @@ module.exports = {
       },
     ],
   ],
-  plugins: [['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }]],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
+  ],
 };
