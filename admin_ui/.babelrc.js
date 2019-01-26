d3462a041506eb2da5d2ec5c249a529a6a42ee60
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-01-26 19:13:34 +0200

    Configure styled jsx with webpack

--- admin_ui/.babelrc.js
+++ admin_ui/.babelrc.js
@@ -17,6 +17,7 @@ module.exports = {
     ],
   ],
   plugins: [
+    'styled-jsx/babel',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-object-rest-spread',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
