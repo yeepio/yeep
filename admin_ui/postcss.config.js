b70c0f9e326341fcac7b06fe42b8cf95fc6ac711
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-01-27 08:56:15 +0200

    Minor refactor on postcss.config.js

--- admin_ui/postcss.config.js
+++ admin_ui/postcss.config.js
@@ -1,5 +1,6 @@
 const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [tailwindcss('./admin_ui/theme/tailwind.js'), require('autoprefixer')],
+  plugins: [tailwindcss('./admin_ui/theme/tailwind.js'), autoprefixer],
 };
