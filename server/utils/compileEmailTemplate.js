d6e2bae8b9e052eb5b3ee32d1fa06057c41274f2
Nick Rossolatos <spidey_nr@hotmail.com>
2019-01-30 20:31:33 +0200

    add loadConfig util

--- server/utils/compileEmailTemplate.js
+++ server/utils/compileEmailTemplate.js
@@ -1,10 +1,16 @@
+import { readFileSync } from 'fs';
+import memoize from 'lodash/memoize';
 import flow from 'lodash/fp/flow';
 import template from 'lodash/template';
 import juice from 'juice';
 
-const compileEmailTemplate = flow(
-  juice,
-  template
+const readFile = (path) => readFileSync(path, 'utf8');
+const compileEmailTemplate = memoize(
+  flow(
+    readFile,
+    juice,
+    template
+  )
 );
 
 export default compileEmailTemplate;
