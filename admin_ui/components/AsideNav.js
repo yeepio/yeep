5d408addd89cfb5c1172db6a11ab10e7502ead8f
Angelos Chaidas <achaidas@gmail.com>
2019-02-18 22:07:32 +0200

    #30: CSS tidy ups in AsideNav

--- admin_ui/components/AsideNav.js
+++ admin_ui/components/AsideNav.js
@@ -2,56 +2,52 @@ import React from 'react';
 
 /**
  * LHS navigation.
- * TODO: use #top for link hrefs here to avoid eslint's "anchor-is-valid" perstering us,
- *  change this when we have proper routes.
  */
 const AsideNav = () => {
   return (
     <nav className="bg-grey-light w-64 flex-no-shrink hidden lg:block">
       <ul className="list-reset">
-        <li>
-          <a href="#top">Dashboard</a>
-        </li>
-        <li>
-          <a href="#top" className="selected text-blue">
-            Organisations
-          </a>
-        </li>
-        <li>
-          <a href="#top">Users</a>
-        </li>
-        <li>
-          <a href="#top">Roles</a>
-        </li>
-        <li>
-          <a href="#top">Sessions</a>
-        </li>
+        {['Dashboard', 'Organisations', 'Users', 'Roles', 'Sessions'].map((menuItem, index) => (
+          <li key={menuItem}>
+            <a href="#top" className={index === 1 ? getMenuStyle('selected') : getMenuStyle()}>
+              {menuItem}
+            </a>
+          </li>
+        ))}
       </ul>
       <style jsx>{`
         nav {
           /* Min height of the nav is the viewport height minus the 4rem of the header */
           min-height: calc(100vh - 4rem);
         }
-        nav a {
-          color: #22292f;
-          text-decoration: none;
-          /*
-              0.8rem ~= 12.8 px + 18px line height == 43.6px which brings us close to
-              the 44px "thumbprint" height we're after
-             */
-          padding: 0.8rem 2rem;
-          display: block;
-          border-bottom: 1px solid white;
-        }
-        nav a:hover {
-          background-color: white;
-        }
-        nav a.selected {
-          background-color: white;
-        }
       `}</style>
     </nav>
   );
+};
+
+/**
+ * Returns a space-separated list of Tailwind CSS classes
+ * to skin the menu items of the <AsideNav/>
+ * @param {string} state - Either "normal" (default) or "selected"
+ * @returns {string} - A space-separated list of Tailwind CSS classes
+ */
+const getMenuStyle = (state = 'normal') => {
+  // The Tailwind CSS classes for all nav items
+  let styles = [
+    'block',
+    'no-underline',
+    'border-b',
+    'border-white',
+    'py-3',
+    'px-8',
+    'hover:bg-white',
+  ];
+  if (state === 'normal') {
+    styles.push('text-black');
+  } else if (state === 'selected') {
+    styles = [...styles, 'text-blue', 'bg-white'];
+  }
+  return styles.join(' ');
 };
 
 export default AsideNav;
