5892a57529bde81aa4bdbc7ac753d3f8dd8c3061
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-03-04 00:42:05 +0200

    Simple store POC

--- admin_ui/app/session/LoginPage.js
+++ admin_ui/app/session/LoginPage.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from '@reach/router';
+import { useObservable } from 'rxjs-hooks';
 import Head from '../../components/Head';
+import Store from '../Store';
 
 const LoginPage = () => {
+  const store = useContext(Store);
+  const user = useObservable(() => store.session.user$);
+  console.log(user);
   return (
     <React.Fragment>
       <Head>
