20c9ec9c87757a026fa52c5866d7673aa19e21af
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-03-09 23:12:41 +0200

    Implement apidoc endpoint; return OpenAPI v.3 formatted JSON

---
+++ server/api/service/apidoc/controller.test.js
@@ -0,0 +1,12 @@
+/* eslint-env jest */
+import request from 'supertest';
+import server from '../../../server';
+
+describe('api/v1/ping', () => {
+  test('responds as expected', async () => {
+    const res = await request(server).get('/api/v1/ping');
+    expect(res.status).toEqual(200);
+    expect(res.type).toMatch(/json/);
+    expect(res.body).toEqual({ ping: 'pong' });
+  });
+});
--- server/api/service/index.js
+++ server/api/service/index.js
@@ -1,8 +1,10 @@
 import Router from 'koa-router';
 import ping from './ping';
+import apidoc from './apidoc';
 
 const router = Router();
 
 router.get('/v1/ping', ping);
+router.get('/v1/apidoc', apidoc);
 
 export default router;
