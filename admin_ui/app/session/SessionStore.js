0a2db18a7d881d38abfb595bfd26d16bd9e655d8
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-03-10 23:44:12 +0200

    Add property to SessionStore to signify pending login

--- admin_ui/app/session/SessionStore.js
+++ admin_ui/app/session/SessionStore.js
@@ -4,12 +4,15 @@ class SessionStore {
   constructor() {
     const cachedUser = localStorage.getItem('session.user');
     this.user$ = new BehaviorSubject(cachedUser ? JSON.parse(cachedUser) : {});
+    this.pendingLogin$ = new BehaviorSubject(false);
   }
 
   login = ({ username, password }) => {
     console.log(`Mock login ${username}:${password}`);
+    // show pending login indicator
+    this.pendingLogin$.next(true);
     // mock login functionality
-    Promise.resolve(() => {
+    setTimeout(() => {
       const user = {
         id: '507f191e810c19729de860ea',
         username: 'wile',
@@ -18,14 +21,16 @@ class SessionStore {
       };
       // update state
       this.user$.next(user);
+      this.pendingLogin$.next(false);
       // cache user in local storage
       localStorage.setItem('session.user', JSON.stringify(user));
-    });
+    }, 2000);
   };
 
   logout = () => {
     // clear state
     this.user$.next({});
+    this.pendingLogin$.next(false);
     // clear cache
     localStorage.removeItem('session.user');
   };
