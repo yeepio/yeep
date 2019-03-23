93e74e599e21178e92097975eaa23ca234055af0
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-03-23 09:53:02 +0200

    Pass full ctx as 1st argument to listOrgs() service

--- server/api/org/list/controller.js
+++ server/api/org/list/controller.js
@@ -37,22 +37,25 @@ const validationSchema = {
 };
 
 const isRequestorAllowedToReadUsers = (requestorPermissions, orgId) => {
-  const hasUserReadPermissions = findUserPermissionIndex(requestorPermissions, {
-    name: 'yeep.user.read',
-    orgId,
-  }) !== -1;
-  const hasPermissionsAssignmentRead = findUserPermissionIndex(requestorPermissions, {
-    name: 'yeep.permission.assignment.read',
-    orgId,
-  }) !== -1;
+  const hasUserReadPermissions =
+    findUserPermissionIndex(requestorPermissions, {
+      name: 'yeep.user.read',
+      orgId,
+    }) !== -1;
+  const hasPermissionsAssignmentRead =
+    findUserPermissionIndex(requestorPermissions, {
+      name: 'yeep.permission.assignment.read',
+      orgId,
+    }) !== -1;
 
-  const hasRoleAssignmentRead = findUserPermissionIndex(requestorPermissions, {
-    name: 'yeep.role.assignment.read',
-    orgId,
-  }) !== -1;
+  const hasRoleAssignmentRead =
+    findUserPermissionIndex(requestorPermissions, {
+      name: 'yeep.role.assignment.read',
+      orgId,
+    }) !== -1;
 
   return hasUserReadPermissions && (hasPermissionsAssignmentRead || hasRoleAssignmentRead);
-}
+};
 
 // if a requestor contains the null org (superuser)  then they can access all of the users scopes.
 const getIntersectingScopes = (requestorScope, userScope) => {
@@ -61,14 +64,15 @@ const getIntersectingScopes = (requestorScope, userScope) => {
   }
 
   return intersection(requestorScope, userScope);
-}
+};
 
 const isUserAuthorised = async ({ request }, next) => {
   // verify a user has access to the requested user
   if (request.body.user) {
     const isUserRequestorIdentical = request.session.user.id === request.body.user;
     const hasPermission = Array.from(new Set([...request.session.requestedUser.orgs, null])).some(
-      (orgId) => isRequestorAllowedToReadUsers(request.session.user.permissions, orgId));
+      (orgId) => isRequestorAllowedToReadUsers(request.session.user.permissions, orgId)
+    );
 
     if (!isUserRequestorIdentical && !hasPermission) {
       throw new AuthorizationError(
@@ -97,24 +101,25 @@ const visitRequestedUser = async ({ request, db }, next) => {
 };
 
 /*
-**  Authorisation Logic
-**  1. User param is not specified
-**    i. Retrieve all orgs requestor has yeep.org.read permission.
-**       Please note null implies all orgs (superuser),
-**    ii. Returns orgs
-**
-**  2. User param is specified
-**    i. Retrieve user orgs by user body param.
-**    ii. Check if requestor has yeep.user.read AND 
-**        (yeep.permission.assignment.read OR yeep.role.assignment.read)
-*         in global scope (null) or at least 1 org from the user's orgs. 
-*         Otherwise return error since the requestor cannot access the specified user.
-**    iii. Return orgs that are the intersection between requestor orgs and user orgs.
+ **  Authorisation Logic
+ **  1. User param is not specified
+ **    i. Retrieve all orgs requestor has yeep.org.read permission.
+ **       Please note null implies all orgs (superuser),
+ **    ii. Returns orgs
+ **
+ **  2. User param is specified
+ **    i. Retrieve user orgs by user body param.
+ **    ii. Check if requestor has yeep.user.read AND
+ **        (yeep.permission.assignment.read OR yeep.role.assignment.read)
+ *         in global scope (null) or at least 1 org from the user's orgs.
+ *         Otherwise return error since the requestor cannot access the specified user.
+ **    iii. Return orgs that are the intersection between requestor orgs and user orgs.
  */
-async function handler({ request, response, db }) {
+async function handler(ctx) {
+  const { request, response } = ctx;
   const { q, limit, user, cursor } = request.body;
   const scopes = getAuthorizedUniqueOrgIds(request, 'yeep.org.read');
-  const orgs = await listOrgs(db, {
+  const orgs = await listOrgs(ctx, {
     q,
     limit,
     cursor: cursor ? parseCursor(cursor) : null,
