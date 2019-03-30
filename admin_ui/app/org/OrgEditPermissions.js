132b8d946a5422f89b5e63c7ae21e10470d38f6d
Angelos Chaidas <achaidas@gmail.com>
2019-03-30 08:17:40 +0200

    OrgEditPermissions page

--- admin_ui/app/org/OrgEditPermissions.js
+++ admin_ui/app/org/OrgEditPermissions.js
@@ -3,6 +3,33 @@ import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
 import useDocumentTitle from '@rehooks/document-title';
 import TabLinks from '../../components/TabLinks';
+import Button from '../../components/Button';
+import Grid from '../../components/Grid';
+
+// Dummy data
+let permissionHeadings = [
+  {
+    label: 'Name',
+    className: 'text-left',
+  },
+  { label: 'Is system?' },
+  { label: 'Role assignments' },
+  { label: 'Actions', isSortable: false },
+];
+let permissionData = [
+  {
+    id: 7,
+    name: 'blog.read',
+    systemPermission: false,
+    roles: 1,
+  },
+  {
+    id: 8,
+    name: 'blog.modify',
+    systemPermission: false,
+    roles: 1,
+  },
+];
 
 const OrgEditPermissions = ({ orgId }) => {
   useDocumentTitle(`Organization name: Permissions`);
@@ -30,9 +57,41 @@ const OrgEditPermissions = ({ orgId }) => {
           },
         ]}
       />
+      <fieldset className="mb-6">
+        <legend>New permissions</legend>
+        <Button>Create new permission</Button>
+        <p className="mt-4">
+          Tip: If you want to create a permission that is <em>not</em> scoped to the
+          &quot;ORGNAME&quot; organization, please{' '}
+          <Link to="/permissions">visit the permissions page</Link>.
+        </p>
+      </fieldset>
+      <fieldset className="mb-6">
+        <legend>Existing permissions</legend>
+        <Grid
+          headings={permissionHeadings}
+          data={permissionData}
+          renderer={(permissionData, index) => {
+            return (
+              <tr key={`permissionRow${index}`} className={index % 2 ? `bg-grey-lightest` : ``}>
+                <td className="p-2">
+                  <Link to={`/permissions/${permissionData.id}/edit`}>{permissionData.name}</Link>
+                </td>
+                <td className="p-2 text-center">{permissionData.systemPermission ? 'Yes' : '-'}</td>
+                <td className="p-2 text-center">{permissionData.roles}</td>
+                <td className="p-2 text-center">
+                  <Link to={`/permissions/${permissionData.id}/edit`}>Edit</Link> <a href="/">Delete</a>
+                </td>
+              </tr>
+            );
+          }}
+        />
+      </fieldset>
       <p className="flex">
         <Link to={`/organizations/${orgId}/edit`}>&laquo; Organization details</Link>
-        <Link to={`/organizations/${orgId}/edit/roles`} className="ml-auto">Roles &raquo;</Link>
+        <Link to={`/organizations/${orgId}/edit/roles`} className="ml-auto">
+          Roles &raquo;
+        </Link>
       </p>
     </React.Fragment>
   );
