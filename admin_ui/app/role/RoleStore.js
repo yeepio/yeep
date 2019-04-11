999b9881dbcf9c52810576831f53525d1f5de8e1
Angelos Chaidas <achaidas@gmail.com>
2019-04-11 06:53:01 +0300

    RoleEditPage => Show the "Save changes" buttons only when org or perms have changed (demo / test)

--- admin_ui/app/role/RoleStore.js
+++ admin_ui/app/role/RoleStore.js
@@ -2,11 +2,13 @@ import { BehaviorSubject } from 'rxjs';
 
 class RoleStore {
   constructor() {
-    /*
-      The deleteModal$ observable takes 3 possible values:
-      "" (empty string), "DELETE", "DELETION_IN_PROGRESS"
-     */
+    // The deleteModal$ observable takes 3 possible values:
+    // "" (empty string), "DELETE", "DELETION_IN_PROGRESS"
     this.deleteModal$ = new BehaviorSubject('');
+    // Useful flag to indicate user has changed the roles organization
+    this.changedOrg$ = new BehaviorSubject(false);
+    // Useful flag to indicate user has changed the roles permissions
+    this.changedPermissions$ = new BehaviorSubject(false);
   }
 }
 
