dc40d894117ea39e1be47bd7b075dd826d5bfcf3
Angelos Chaidas <achaidas@gmail.com>
2019-04-11 09:16:49 +0300

    OrgEditRolesModals => Add permissions multi-select

--- admin_ui/app/org/OrgEditRolesModals.js
+++ admin_ui/app/org/OrgEditRolesModals.js
@@ -6,6 +6,7 @@ import Modal from '../../components/Modal';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Textarea from '../../components/Textarea';
+import Select from 'react-select';
 
 // The CREATE, EDIT or DELETE modals for the OrgEditPermissions page
 const OrgEditRolesModals = () => {
@@ -19,7 +20,7 @@ const OrgEditRolesModals = () => {
   );
 
   const handleESC = (e) => {
-    if (e.key === "Escape") {
+    if (e.key === 'Escape') {
       // ESC key pressed. Hide any modals
       store.org.currentRolesModal$.next('');
     }
@@ -62,7 +63,26 @@ const OrgEditRolesModals = () => {
         <div className="sm:flex mb-4">
           <div className="sm:w-1/4">Permissions:</div>
           <div className="sm:w-3/4">
-            <em className="text-grey">pillboxes? or select dropdown perm picker? TBD</em>
+            <Select
+              isMulti
+              className="w-full mb-4"
+              placeholder="Choose one or more permissions"
+              options={[
+                { value: 1, label: 'blog.read' },
+                { value: 2, label: 'blog.write' },
+                { value: 3, label: 'yeep.org.write' },
+                { value: 4, label: 'yeep.org.read' },
+                { value: 5, label: 'yeep.users.read' },
+                { value: 6, label: 'yeep.users.write' },
+                { value: 7, label: 'yeep.someperm.1' },
+                { value: 8, label: 'yeep.someperm.2' },
+                { value: 9, label: 'yeep.someperm.2' },
+                { value: 10, label: 'crm.access.1', isDisabled: true },
+                { value: 11, label: 'crm.access.2', isDisabled: true },
+                { value: 12, label: 'crm.access.3', isDisabled: true },
+              ]}
+              defaultValue={[{ value: 1, label: 'blog.read' }, { value: 2, label: 'blog.write' }]}
+            />
           </div>
         </div>
         <div className="form-submit">
@@ -94,7 +114,26 @@ const OrgEditRolesModals = () => {
         <div className="sm:flex mb-4">
           <div className="sm:w-1/4">Permissions:</div>
           <div className="sm:w-3/4">
-            <em className="text-grey">pillboxes? or select dropdown perm picker? TBD</em>
+            <Select
+              isMulti
+              className="w-full mb-4"
+              placeholder="Choose one or more permissions"
+              options={[
+                { value: 1, label: 'blog.read' },
+                { value: 2, label: 'blog.write' },
+                { value: 3, label: 'yeep.org.write' },
+                { value: 4, label: 'yeep.org.read' },
+                { value: 5, label: 'yeep.users.read' },
+                { value: 6, label: 'yeep.users.write' },
+                { value: 7, label: 'yeep.someperm.1' },
+                { value: 8, label: 'yeep.someperm.2' },
+                { value: 9, label: 'yeep.someperm.2' },
+                { value: 10, label: 'crm.access.1', isDisabled: true },
+                { value: 11, label: 'crm.access.2', isDisabled: true },
+                { value: 12, label: 'crm.access.3', isDisabled: true },
+              ]}
+              defaultValue={[{ value: 1, label: 'blog.read' }, { value: 2, label: 'blog.write' }]}
+            />
           </div>
         </div>
         <div className="form-submit">
