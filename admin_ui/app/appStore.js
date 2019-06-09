b3caa19d194a7c34405020754189b99a45060fe2
Angelos Chaidas <achaidas@gmail.com>
2019-06-09 22:35:56 +0300

    Turn on Redux dev tools for non-production environments

--- admin_ui/app/appStore.js
+++ admin_ui/app/appStore.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import {
   reducer as sessionReducer,
@@ -22,13 +22,24 @@ const preloadedState = {
   role: roleInitialState,
 };
 
+// We need ReduxThunk in order to enable async flows in Redux
 const middleware = [ReduxThunk];
+let enhancer = applyMiddleware(...middleware);
 
+// Turn on a few more middleware goodies when we're not in proudction
 if (process.env.NODE_ENV !== 'production') {
   const { createLogger } = require('redux-logger'); // eslint-disable-line global-require
   const freeze = require('redux-freeze');
-
   middleware.push(createLogger({ level: 'info', collapsed: true }), freeze);
+  // Let's also enable the use of the Redux devtool extension
+  // (lovely visualisation of state plus time travel!)
+  const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        })
+      : compose;
+  enhancer = composeEnhancers(applyMiddleware(...middleware));
 }
 
-export const store = createStore(rootReducer, preloadedState, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, preloadedState, enhancer);
