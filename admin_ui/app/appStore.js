1b6c4f84291afc06f08c44bea1b720f01d550afb
Angelos Chaidas <achaidas@gmail.com>
2019-06-10 11:55:36 +0300

    Better implementation of Redux dev tools code

--- admin_ui/app/appStore.js
+++ admin_ui/app/appStore.js
@@ -22,24 +22,29 @@ const preloadedState = {
   role: roleInitialState,
 };
 
-// We need ReduxThunk in order to enable async flows in Redux
-const middleware = [ReduxThunk];
-let enhancer = applyMiddleware(...middleware);
+const middleware = [
+  ReduxThunk, // we need ReduxThunk to enable async flows in Redux
+];
+
+let composeEnhancers = compose;
 
 // Turn on a few more middleware goodies when we're not in proudction
 if (process.env.NODE_ENV !== 'production') {
   const { createLogger } = require('redux-logger'); // eslint-disable-line global-require
   const freeze = require('redux-freeze');
   middleware.push(createLogger({ level: 'info', collapsed: true }), freeze);
+
   // Let's also enable the use of the Redux devtool extension
   // (lovely visualisation of state plus time travel!)
-  const composeEnhancers =
-    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-        })
-      : compose;
-  enhancer = composeEnhancers(applyMiddleware(...middleware));
+  if (typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    });
+  }
 }
 
-export const store = createStore(rootReducer, preloadedState, enhancer);
+export const store = createStore(
+  rootReducer,
+  preloadedState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
