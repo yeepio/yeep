3b5623c90ed8f3b379dc9ebcbd8f88930b6be06c
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-07-29 23:30:21 +0300

    Force user logout on yeep authorization error

--- admin_ui/app/yeepClient.js
+++ admin_ui/app/yeepClient.js
@@ -1,8 +1,20 @@
 import YeepClient from '@yeep/client';
+import { store } from './appStore';
+import { navigate } from '@reach/router';
+import { resolveLogout } from './session/sessionStore';
 
 const yeepClient = new YeepClient({
   baseURL: process.env.API_BASE_URL,
-  onError: (err) => console.error(err),
+  onError: (err) => {
+    switch (err.code) {
+      case 10012: {
+        // AuthorizationError
+        store.dispatch(resolveLogout());
+        navigate('/login');
+        break;
+      }
+    }
+  },
 });
 
 export default yeepClient;
