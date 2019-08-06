6765b7d8ac0d63a7d139a8560f6ed491d0df4e2e
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-08-06 09:10:41 +0300

    Refactor role.list() service; fix issue with count query

--- server/api/role/list/controller.js
+++ server/api/role/list/controller.js
@@ -60,7 +60,7 @@ async function handler(ctx) {
   const { request, response } = ctx;
   const { q, limit, cursor, isSystemRole, scope } = request.body;
 
-  const { roles, rolesCount } = await listRoles(ctx, {
+  const { roles, roleCount } = await listRoles(ctx, {
     q,
     limit,
     cursor: cursor ? parseCursor(cursor) : null,
@@ -71,7 +71,7 @@ async function handler(ctx) {
   response.status = 200; // OK
   response.body = {
     roles,
-    rolesCount,
+    roleCount,
     nextCursor: roles.length < limit ? undefined : stringifyCursor(last(roles)),
   };
 }
