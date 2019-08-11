2524e90dbdc215532aed83f70d58f72f686b401c
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-08-11 10:08:16 +0300

    Update fixtures/load.js to convert external keys to ObjectId

--- fixtures/load.js
+++ fixtures/load.js
@@ -13,15 +13,17 @@ const readFileAsync = promisify(fs.readFile);
 const connectDb = async (uri) => {
   const client = await MongoClient.connect(uri, { useNewUrlParser: true });
   return client;
-}
+};
 
 const getAdminRole = async (db) => {
   const adminRole = db.collection('roles').findOne({ name: 'admin' });
   if (!adminRole) {
-    throw new Error('Admin Role does not already exist in the database, please make sure you have run all the correct migrations');
+    throw new Error(
+      'Admin Role does not already exist in the database, please make sure you have run all the correct migrations'
+    );
   }
   return adminRole;
-}
+};
 
 const saveAdminUser = async (db, user) => {
   const salt = await authFactor.generateSalt();
@@ -40,69 +42,84 @@ const saveAdminUser = async (db, user) => {
   db.collection('orgMemberships').insertOne({
     userId: ObjectId(user.id),
     orgId: null,
-    roles: [{
-      id: adminRole._id,
-    }],
+    roles: [
+      {
+        id: adminRole._id,
+      },
+    ],
     isFixture: true,
   });
-}
+};
 
 const saveUsers = async (db, users) => {
-  let bulk = db.collection('users').initializeUnorderedBulkOp();
+  const bulk = db.collection('users').initializeUnorderedBulkOp();
+  const now = new Date();
+
   users.forEach((user) => {
-    // later used for identifying all fixture data
-    user.isFixture = true;
-    user.createdAt = new Date();
-    user.updatedAt = new Date();
+    user.isFixture = true; // later used for identifying all fixture data
+    user.createdAt = now;
+    user.updatedAt = now;
     // we need to create credentials for this password if we need to keep it
     const { password, ...dbUser } = user; // eslint-disable-line
     bulk.insert(dbUser);
   });
+
   const operation = await bulk.execute();
   return operation.result.insertedIds.map((user) => user._id.toString());
 };
 
 const saveOrgs = async (db, orgs) => {
-  let bulk = db.collection('orgs').initializeUnorderedBulkOp();
+  const bulk = db.collection('orgs').initializeUnorderedBulkOp();
+  const now = new Date();
+
   orgs.forEach((org) => {
-    // later used for identifying all fixture data
-    org.isFixture = true;
-    org.createdAt = new Date();
-    org.updatedAt = new Date();
+    org.isFixture = true; // later used for identifying all fixture data
+    org.createdAt = now;
+    org.updatedAt = now;
     bulk.insert(org);
   });
+
   const operation = await bulk.execute();
   return operation.result.insertedIds.map((org) => org._id.toString());
 };
 
 const savePermissions = async (db, permissions) => {
-  let bulk = db.collection('permissions').initializeUnorderedBulkOp();
+  const bulk = db.collection('permissions').initializeUnorderedBulkOp();
+  const now = new Date();
+
   permissions.forEach((permission) => {
-    // later used for identifying all fixture data
-    permission.isFixture = true;
-    permission.createdAt = new Date();
-    permission.updatedAt = new Date();
+    permission.scope = permission.scope ? ObjectId(permission.scope) : null;
+    permission.isFixture = true; // later used for identifying all fixture data
+    permission.createdAt = now;
+    permission.updatedAt = now;
     bulk.insert(permission);
   });
+
   const operation = await bulk.execute();
   return operation.result.insertedIds.map((permission) => permission._id.toString());
 };
 
 const saveRoles = async (db, roles) => {
-  let bulk = db.collection('roles').initializeUnorderedBulkOp();
+  const bulk = db.collection('roles').initializeUnorderedBulkOp();
+  const now = new Date();
+
   roles.forEach((role) => {
-    // later used for identifying all fixture data
-    role.isFixture = true;
-    role.createdAt = new Date();
-    role.updatedAt = new Date();
+    role.permissions = role.permissions.map(ObjectId);
+    role.scope = role.scope ? ObjectId(role.scope) : null;
+    role.isFixture = true; // later used for identifying all fixture data
+    role.createdAt = now;
+    role.updatedAt = now;
     bulk.insert(role);
   });
+
   const operation = await bulk.execute();
   return operation.result.insertedIds.map((role) => role._id.toString());
 };
 
 const saveOrgMemberships = async (db, userIds) => {
-  let bulk = db.collection('orgMemberships').initializeUnorderedBulkOp();
+  const bulk = db.collection('orgMemberships').initializeUnorderedBulkOp();
+  const now = new Date();
+
   userIds.forEach((userId) => {
     const orgMembership = {
       userId: ObjectId(userId),
@@ -111,13 +128,14 @@ const saveOrgMemberships = async (db, userIds) => {
       permissions: [],
       expiresAt: null,
       isFixture: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }
+      createdAt: now,
+      updatedAt: now,
+    };
     bulk.insert(orgMembership);
   });
-  const operation = await bulk.execute();
-}
+
+  await bulk.execute();
+};
 
 const loadFixtures = async (config, inputPath) => {
   const dataPath = inputPath || path.join(__dirname, '../fixtures/data/data.json');
@@ -139,7 +157,7 @@ const loadFixtures = async (config, inputPath) => {
     await saveAdminUser(db, adminUser);
 
     const orgIds = await saveOrgs(db, data.orgs);
-    
+
     const editedPermissions = data.permissions.map((permission) => {
       const orgIndex = data.orgs.findIndex((org) => org.slug === permission.scope);
       permission.scope = orgIds[orgIndex];
