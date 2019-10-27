9055c32369f9c7fd2b4d6f8d7b6bfbb58e0d7ce4
Angelos Chaidas <achaidas@gmail.com>
2019-10-27 10:40:44 +0200

    51: Implement logic for showing Onboarding Dashboard UI

---
+++ admin_ui/app/dashboard/DashboardOnboarding.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashboardOnboarding = () => {
+  return (
+    <p>Yay! onboard....</p>
+  );
+};
+
+export default DashboardOnboarding;
--- admin_ui/app/dashboard/DashboardPage.js
+++ admin_ui/app/dashboard/DashboardPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useDocumentTitle from '@rehooks/document-title';
+import DashboardOnboarding from './DashboardOnboarding';
 import IconOrganisation from '../../icons/IconOrganisation';
 import IconUser from '../../icons/IconUser';
 import ButtonLink from '../../components/ButtonLink';
@@ -13,8 +14,6 @@ import Spinner from '../../components/Spinner';
 
 const DashboardPage = () => {
   useDocumentTitle('Dashboard');
-  console.log('render!');
-
   const dispatch = useDispatch();
 
   // TODO:
@@ -28,7 +27,9 @@ const DashboardPage = () => {
   const permissionCount = useSelector((state) => state.permission.list.totalCount);
   // TODO: Populate the two varibles beow with proper selectors
   //  once branch user-adminui-management is merged
-  const userCountLoading = true;
+  const userCountLoading = false;
+  // Temporary: Change to 1 to view the "normal" dashboard view.
+  // Keep as 0 to view the onboarding UI
   const userCount = 0;
 
   React.useEffect(() => {
@@ -42,60 +43,76 @@ const DashboardPage = () => {
   return (
     <div className="leading-normal p-4 sm:p-8 max-w-2xl sm:h-full">
       <h1 className="mb-4 font-semibold text-3xl">Dashboard</h1>
-      <p className="mb-4">
-        Welcome <strong>{userFullName}</strong>. You Yeep installation is currently helping you
-        manage the following:
-      </p>
-      <div className="sm:flex flex-wrap">
-        <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
-          <IconOrganisation className="inline-block mb-4" color="#8492A6" height={44} />
-          <h2 className="mb-4 text-2xl">
-            {orgCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
-            {!orgCountLoading && (
-              <React.Fragment>
-                <strong>{orgCount}</strong> organizations
-              </React.Fragment>
-            )}
-          </h2>
-          <ButtonLink to="organizations">View all organizations</ButtonLink>
-        </div>
-        <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
-          <IconUser className="inline-block mb-4" color="#8492A6" height={44} />
-          <h2 className="mb-4 text-2xl">
-            {userCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
-            {!userCountLoading && (
-              <React.Fragment>
-                <strong>{userCount}</strong> users
-              </React.Fragment>
-            )}
-          </h2>
-          <ButtonLink to="users">View all users</ButtonLink>
-        </div>
-        <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
-          <IconPermission className="inline-block mb-4" color="#8492A6" height={44} />
-          <h2 className="mb-4 text-2xl">
-            {permissionCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
-            {!permissionCountLoading && (
-              <React.Fragment>
-                <strong>{permissionCount}</strong> permissions
-              </React.Fragment>
-            )}
-          </h2>
-          <ButtonLink to="permissions">View all permissions</ButtonLink>
-        </div>
-        <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
-          <IconRole className="inline-block mb-4" color="#8492A6" height={44} />
-          <h2 className="mb-4 text-2xl">
-            {roleCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
-            {!roleCountLoading && (
-              <React.Fragment>
-                <strong>{roleCount}</strong> roles
-              </React.Fragment>
-            )}
-          </h2>
-          <ButtonLink to="roles">View all roles</ButtonLink>
-        </div>
-      </div>
+      {(orgCountLoading || roleCountLoading || permissionCountLoading || userCountLoading) && (
+        <Spinner className="ml-auto mr-auto" />
+      )}
+      {!orgCountLoading &&
+        !roleCountLoading &&
+        !permissionCountLoading &&
+        !userCountLoading &&
+        (userCount > 0 && orgCount > 0) && (
+          <React.Fragment>
+            <p className="mb-4">
+              Welcome <strong>{userFullName}</strong>. You Yeep installation is currently helping
+              you manage the following:
+            </p>
+            <div className="sm:flex flex-wrap">
+              <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
+                <IconOrganisation className="inline-block mb-4" color="#8492A6" height={44} />
+                <h2 className="mb-4 text-2xl">
+                  {orgCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
+                  {!orgCountLoading && (
+                    <React.Fragment>
+                      <strong>{orgCount}</strong> organizations
+                    </React.Fragment>
+                  )}
+                </h2>
+                <ButtonLink to="organizations">View all organizations</ButtonLink>
+              </div>
+              <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
+                <IconUser className="inline-block mb-4" color="#8492A6" height={44} />
+                <h2 className="mb-4 text-2xl">
+                  {userCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
+                  {!userCountLoading && (
+                    <React.Fragment>
+                      <strong>{userCount}</strong> users
+                    </React.Fragment>
+                  )}
+                </h2>
+                <ButtonLink to="users">View all users</ButtonLink>
+              </div>
+              <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
+                <IconPermission className="inline-block mb-4" color="#8492A6" height={44} />
+                <h2 className="mb-4 text-2xl">
+                  {permissionCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
+                  {!permissionCountLoading && (
+                    <React.Fragment>
+                      <strong>{permissionCount}</strong> permissions
+                    </React.Fragment>
+                  )}
+                </h2>
+                <ButtonLink to="permissions">View all permissions</ButtonLink>
+              </div>
+              <div className="border border-grey rounded p-4 sm:mr-4 flex-auto mb-4 text-center">
+                <IconRole className="inline-block mb-4" color="#8492A6" height={44} />
+                <h2 className="mb-4 text-2xl">
+                  {roleCountLoading && <Spinner size={36} className="ml-auto mr-auto" />}
+                  {!roleCountLoading && (
+                    <React.Fragment>
+                      <strong>{roleCount}</strong> roles
+                    </React.Fragment>
+                  )}
+                </h2>
+                <ButtonLink to="roles">View all roles</ButtonLink>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+      {!orgCountLoading &&
+        !roleCountLoading &&
+        !permissionCountLoading &&
+        !userCountLoading &&
+        (userCount === 0 || orgCount === 0) && <DashboardOnboarding />}
     </div>
   );
 };
