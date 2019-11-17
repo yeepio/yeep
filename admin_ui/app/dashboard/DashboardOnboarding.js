e15b57e195e64b482edc72701576d7130f528130
Angelos Chaidas <achaidas@gmail.com>
2019-11-17 20:02:31 +0200

    51: Finalise styling for OnboardingPane component

--- admin_ui/app/dashboard/DashboardOnboarding.js
+++ admin_ui/app/dashboard/DashboardOnboarding.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
+import classNames from 'classnames';
 
 const DashboardOnboarding = ({ orgCount, permissionCount, roleCount, userCount }) => {
   return (
@@ -15,28 +16,29 @@ const DashboardOnboarding = ({ orgCount, permissionCount, roleCount, userCount }
         link="/organizations/create"
         linkText="Create an organisation"
         description='An organisation is the "umbrella" under which users, permissions (and roles) will be grouped. You can create as many organisations as you want. Users can belong to as many organisations as you see fit.'
+        successText={orgCount ? `${orgCount} organisations have been created` : null}
       />
-      <div className="rounded-lg p-4 flex md:items-center mb-4 bg-green-lightest">
-        <div className="mr-3 w-16 h-16 flex-none bg-green-dark flex rounded rounded-full justify-center items-center rounded">
-          <span className="text-3xl text-white">
-            <img src="/icon-yes-white.svg" width="36" height="29" alt="Checkmark" />
-          </span>
-        </div>
-        <div>
-          <Link to="organizations/create" className="text-2xl font-bold text-green-dark">
-            Create an organisation
-          </Link>
-          <p className="text-green-dark md:float-right">Great! 10 permissions have been created</p>
-          <p>
-            An organisation is the &quot;umbrella&quot; under which users, permissions (and roles)
-            will be grouped. You can create as many organisations as you want. Users can belong to
-            as many organisations as you see fit.
-          </p>
-        </div>
-      </div>
-      <p>
-        {orgCount} - {permissionCount} - {roleCount} - {userCount}
-      </p>
+      <OnboardingPane
+        number={2}
+        link="/permissions/create"
+        linkText="Create one (or more) permissions"
+        description='A permission is meant to be assigned to a user and help you identify what they can and can not do. Permissions can be scoped to an organisation or be "global". For example, a "Blog" organisation would most probably have permissions like "blog_read", "blog_write", "blog_administer".'
+        successText={permissionCount ? `${permissionCount} permissions have been created` : null}
+      />
+      <OnboardingPane
+        number={3}
+        link="/roles/create"
+        linkText="Create roles (optional step)"
+        description='A role is a grouping of permissions and allows an admin to quickly assign multiple permissions to a user. For example, a "Blog editor" role could contain the "blog_read" and "blog_write" permissions but not a "blog_administer" one.'
+        successText={roleCount ? `${roleCount} roles have been created` : null}
+      />
+      <OnboardingPane
+        number={4}
+        link="/users/create"
+        linkText="Create (or invite) users"
+        description="Finally you can create your users and assign them to one or more organisations with the appropriate roles and / or permissions. You can also partially create profiles and invite the users via email to log in and create their profiles."
+        successText={userCount ? `${userCount} users have been created` : null}
+      />
     </React.Fragment>
   );
 };
@@ -50,14 +52,62 @@ DashboardOnboarding.propTypes = {
 
 export default DashboardOnboarding;
 
-const OnboardingPane = ({ number, link, linkText, description, successText }) => {
+const OnboardingPane = ({ number, link, linkText, description, successText = null }) => {
   return (
-    <div className="border border-grey rounded-lg p-4 flex md:items-center mb-4">
-      <OnboardingNumber number={number} />
+    <div
+      className={classNames(
+        {
+          border: successText === null,
+          'border-grey': successText === null,
+        },
+        {
+          'bg-green-lightest': successText !== null,
+        },
+        'rounded-lg',
+        'p-4',
+        'flex',
+        'md:items-center',
+        'mb-4'
+      )}
+    >
+      <div
+        className={classNames(
+          'mr-3',
+          'w-16',
+          'h-16',
+          'flex-none',
+          'flex',
+          'rounded',
+          'rounded-full',
+          'justify-center',
+          'items-center',
+          {
+            'bg-grey': successText === null,
+          },
+          {
+            'bg-green-dark': successText !== null,
+          }
+        )}
+      >
+        <span className="text-3xl text-white">
+          {successText ? (
+            <img src="/icon-yes-white.svg" width="36" height="29" alt="Checkmark" />
+          ) : (
+            number
+          )}
+        </span>
+      </div>
       <div>
-        <Link to={link} className="text-2xl font-bold">
+        <Link
+          to={link}
+          className={classNames('text-2xl', 'font-bold', {
+            'text-green-dark': successText,
+            'hover:text-green-darker': successText,
+          })}
+        >
           {linkText}
         </Link>
+        {successText && <p className="text-green-dark md:float-right">{successText}</p>}
         <p>{description}</p>
       </div>
     </div>
@@ -68,20 +118,5 @@ OnboardingPane.propTypes = {
   link: PropTypes.string.isRequired,
   linkText: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  successText: PropTypes.string
-};
-
-/**
- * Helper sub-component, outputs the large number-in-a-circle inside each OnboardingPane
- * @param number
- */
-const OnboardingNumber = ({ number }) => {
-  return (
-    <div className="mr-3 w-16 h-16 flex-none bg-grey flex rounded rounded-full justify-center items-center rounded">
-      <span className="text-3xl text-white">{number}</span>
-    </div>
-  );
-};
-OnboardingNumber.propTypes = {
-  number: PropTypes.number.isRequired,
+  successText: PropTypes.string,
 };
